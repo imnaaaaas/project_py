3e138ee219f8f54a52ec5a0a0bb24a552f702e14
imnaaaaas <arevadzeluka175@gmail.com>
2026-08-04 13:11:16 +0400

    Update tests

--- tests/test_priority_queue.py
+++ tests/test_priority_queue.py
@@ -7,34 +7,34 @@ from transit.priority_queue import PriorityQueue
 
 
 
-def test_insert_and_extract_min():#does it return smallest 1 
+def test_insert_and_pop_min():#does it return smallest 1 
     pq = PriorityQueue()
-    pq.insert_item(10, "A")
-    pq.insert_item(5, "B")
-    pq.insert_item(8, "C")
+    pq.push(10, "A")
+    pq.push(5, "B")
+    pq.push(8, "C")
 
-    assert pq.extract_min() == [5,"B"]
-    assert pq.extract_min() == [8,"C"]
-    assert pq.extract_min() == [10,"A"]
+    assert pq.pop_min() == [5,"B"]
+    assert pq.pop_min() == [8,"C"]
+    assert pq.pop_min() == [10,"A"]
 
 
 
-def test_extract_min_on_empty_queue(): #fail safely or crash 2
+def test_pop_min_on_empty_queue(): #fail safely or crash 2
     pq = PriorityQueue()
-    assert pq.extract_min() == None
+    assert pq.pop_min() == None
 
 
 
 def test_dublicates_extracted_correctly(): #when things are tie 3
     pq = PriorityQueue()
-    pq.insert_item(10, "A")
-    pq.insert_item(10,"B")
-    pq.insert_item(5,"C")
+    pq.push(10, "A")
+    pq.push(10,"B")
+    pq.push(5,"C")
 
 
-    first =  pq.extract_min() 
-    second =pq.extract_min()
-    third=pq.extract_min()
+    first =  pq.pop_min() 
+    second =pq.pop_min()
+    third=pq.pop_min()
 
     assert first == [5,"C"]
     assert {second[1], third[1]} =={"A","B"}
@@ -43,15 +43,15 @@ def test_dublicates_extracted_correctly(): #when things are tie 3
 
 def test_decrease_ley_changes_extraction_order():#lower priority change extraction order(decrease_key)
     pq = PriorityQueue()
-    pq.insert_item(10, "A")
-    pq.insert_item(5, "B")
-    pq.insert_item(8, "C")
+    pq.push(10, "A")
+    pq.push(5, "B")
+    pq.push(8, "C")
 
     pq.decrease_key("A",3)
 
-    assert pq.extract_min() == [3,"A"]
-    assert pq.extract_min() == [5,"B"]
-    assert pq.extract_min() == [8,"C"]
+    assert pq.pop_min() == [3,"A"]
+    assert pq.pop_min() == [5,"B"]
+    assert pq.pop_min() == [8,"C"]
 
 def test_decrease_key_missing_elemet(): #fail visible or do undefiened 6
     pq = PriorityQueue()
@@ -60,9 +60,9 @@ def test_decrease_key_missing_elemet(): #fail visible or do undefiened 6
 
 def test_decrease_key_larger_value_ignored(): #equal/larger value 7 bad value
     pq = PriorityQueue()
-    pq.insert_item(5, "A")
+    pq.push(5, "A")
     pq.decrease_key("A",10)
-    assert pq.extract_min() == [5,"A"]
+    assert pq.pop_min() == [5,"A"]
 
 
 def test_strees_again_heapq(): #  8
@@ -77,9 +77,9 @@ def test_strees_again_heapq(): #  8
     reference = [] 
 
     for priority,stop_id in items:
-        pq.insert_item(priority,stop_id)
+        pq.push(priority,stop_id)
         heapq.heappush(reference,[priority,stop_id])
     for _ in range(50):
-        mine = pq.extract_min()
+        mine = pq.pop_min()
         heq = heapq.heappop(reference)
         assert mine == heq
